5fa7de93d78b3a1e32175883bd3628a7fa0db31c
Ddilibe <franklinfidelugwuowo@gmail.com>
2026-01-20 21:08:42 +0100

    fix: fixed wpdb error

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -218,6 +218,7 @@ class UserController
 
 
     public static function allCertificate() {
+        global $wpdb;
         $certificates = $wpdb->get_results("SELECT * FROM {$CISON_CERT_TABLE}", ARRAY_A);
         return rest_ensure_response(["data"=>$certificates, "status"=>"success"]);
     }
