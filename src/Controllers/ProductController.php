4d4fbb57fb83207627d4494ff501da1ce7955bec
Ddilibe <franklinfidelugwuowo@gmail.com>
2026-01-22 11:15:14 +0100

    chore: added conference and preconference endpoint

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -36,9 +36,9 @@ class ProductController {
 
         $body = $request->get_json_params();
         $user_id = isset($body['user_id']) ? sanitize_text_field($body['user_id']) : '';
-        $product_id = isset($body['product_id']) ?intval($body['product_id']) : NAN;
+        $product_id = isset($body['product_id']) ?intval($body['product_id']) : 0;
 
-        if (!$user_id | $product_id === NAN) {
+        if (!$user_id | $product_id === 0) {
             return new WP_Error('not_found', 'Member ID not found', ['status' => 404]);
         }
         $has_bought = wc_customer_bought_product('', $user_id, $product_id);
