e0ffb9851de5d3e986cb7344e2870489752d2627
Ddilibe <franklinfidelugwuowo@gmail.com>
2026-01-22 13:54:12 +0100

    chore: added get single cert endpoint

--- src/Controllers/CertController.php
+++ src/Controllers/CertController.php
@@ -124,4 +124,39 @@ public static function addNewCertification(WP_REST_REQUEST $request) {
         'status' => 'success'
     ]);
 }
+    public static function singleCertificate(WP_REST_REQUEST $request) {
+        global $wpdb;
+        $body = $request->get_json_params();
+        $user_id = isset($body['user_id']) ? sanitize_text_field($body['user_id']) : '';
+
+        $member_id = isset($body['member_id']) ? sanitize_text_field($body['member_id']) : '';
+
+        if (empty($user_id) && empty($member_id)) {
+            return new WP_Error('invalid_id', 'User ID or Member ID is required', ['status' => 400]);
+        }
+
+        if (empty($user_id) && $member_id) {
+            $table_name = $wpdb->prefix . 'bp_xprofile_data';
+            $user_id = $wpdb->get_var($wpdb->prepare(
+                "SELECT user_id FROM {$table_name} WHERE field_id = %d AND value = %s LIMIT 1",
+                894,
+                $member_id
+            ));
+        }
+
+        $cert_table_name = CISON_CERT_TABLE;
+        $row = $wpdb->get_row($wpdb->prepare(
+            "SELECT * FROM {$cert_table_name} WHERE user_id = %d LIMIT 1",
+            $user_id
+        ));
+
+        if (empty($row)) {
+            return new WP_Error('not_found', 'Certificate not found', ['status' => 404]);
+        }
+
+        return rest_ensure_response([
+            'data'=> $row,
+            'status'=> 'success'
+        ], 200);
+    }
 }
