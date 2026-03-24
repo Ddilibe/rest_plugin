a071b633278b6768629493d60641a661fc151181
Ddilibe <franklinfidelugwuowo@gmail.com>
2026-03-24 15:26:05 +0100

    feat: created endpoints for retrieving latest users with complete, partial and incomplete payment

--- src/Routes/DataRoute.php
+++ src/Routes/DataRoute.php
@@ -30,5 +30,23 @@ class DataRoute
             "callback" => [DataController::class, "users_without_payments_2025_limit"],
             "permission_callback" => [Auth::class, "jwt"]
         ]);
+
+        register_rest_route($root_api, '/users/complete-payment-latest', [
+            "methods" => "GET",
+            "callback" => [DataController::class, "users_with_cleared_payments"],
+            "permission_callback" => [Auth::class, "jwt"]
+        ]);
+
+        register_rest_route($root_api, '/users/partial-payment-latest', [
+            "methods" => "GET",
+            "callback" => [DataController::class, "users_with_partial_payments"],
+            "permission_callback" => [Auth::class, "jwt"]
+        ]);
+
+        register_rest_route($root_api, "/users/no-payment-latest", [
+            "methods" => "GET",
+            "callback" => [DataController::class, "users_without_payments"],
+            "permission_callback" => [Auth::class, "jwt"]
+        ]);
     }
 }
